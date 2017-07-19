4db8f5382f8779ab2c667b89fb4f7967a5359206
safronoven <Gjhjcz1983>
2017-07-19 15:10:25 +0600

    HW1Optional

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -14,7 +14,11 @@ public class Meal {
 
     private int calories;
 
-   // public Meal(int id, LocalDateTime dateTime, String description, int calories) {
+    public Meal(int i, LocalDateTime of, String завтрак, int i1) {
+
+    }
+
+    // public Meal(int id, LocalDateTime dateTime, String description, int calories) {
        // this.id = id;
        // this.dateTime = dateTime;
        // this.description = description;
